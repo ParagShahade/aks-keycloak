816ee19354d3e075321f7ee1dcb57381374dfbb0
parag <parag.shahade@gmail.com>
2025-07-09 14:06:33 +0200

    updated the backend-tf-state

--- aks-cluster/backend.tf
+++ aks-cluster/backend.tf
@@ -2,5 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "my-tf-rg"
     storage_account_name = "mytfstateacct"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
