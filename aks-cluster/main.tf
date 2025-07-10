1057ddb4d33e8ffcb432536b7996cf358750ac85
parag <parag.shahade@gmail.com>
2025-07-10 19:52:25 +0200

    delete cluster

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+/*resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location
 }
@@ -19,3 +19,4 @@ resource "azurerm_kubernetes_cluster" "example" {
     type = "SystemAssigned"
   }
 }
+*/
